11c0bcfefe9270e274f9db8a7d63022870e000d7
Sarah Lensing <sarahlensing@gmail.com>
2012-03-27 00:57:12 -0400

    NSArray methods using blocks

---
+++ iObjectiveSee/Extensions/NSArray+iOS.h
@@ -0,0 +1,18 @@
+//
+//  NSArray+NSArray_iOS.h
+//  iObjectiveSee
+//
+//  Created by Sarah Lensing on 3/27/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (iOS)
+
+- (void)each:(void (^)(id))block;
+- (id)detect:(BOOL (^)(id))block;
+- (NSArray *)collect:(id (^)(id obj))block;
+- (NSArray *)reject:(BOOL (^)(id obj))block;
+
+@end
---
+++ iObjectiveSee/Extensions/NSArray+iOS.m
@@ -0,0 +1,49 @@
+//
+//  NSArray+NSArray_iOS.m
+//  iObjectiveSee
+//
+//  Created by Sarah Lensing on 3/27/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import "NSArray+iOS.h"
+
+@implementation NSArray (iOS)
+
+- (void)each:(void (^)(id obj))block {
+    for (id _inObj in self) {
+        block(_inObj);
+    }
+}
+
+- (id)detect:(BOOL (^)(id obj))block {
+    for (id _inObj in self) {
+        if (block(_inObj)) {
+            return _inObj;
+        }
+    }
+    return nil;
+}
+
+- (NSArray *)collect:(id (^)(id obj))block {
+    NSMutableArray *ret = [NSMutableArray array];
+    for (id _inObj in self) {
+        id toAdd = block(_inObj);
+        if (toAdd) {
+            [ret addObject:toAdd];
+        }
+    }
+    return ret;
+}
+
+- (NSArray *)reject:(BOOL (^)(id obj))block {
+    NSMutableArray *ret = [NSMutableArray array];
+    for (id _inObj in self) {
+        if (!block(_inObj)) {
+            [ret addObject:_inObj];
+        }
+    }
+    return ret;
+}
+
+@end
