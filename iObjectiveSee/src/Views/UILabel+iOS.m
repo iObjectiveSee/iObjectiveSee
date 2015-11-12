5fa78bd372431024c6fe5311f69cb174278bf4d7
Sarah Lensing <sarahlensing@gmail.com>
2015-11-12 16:31:25 -0500

    Migrate label size methods

--- iObjectiveSee/src/Views/UILabel+iOS.m
+++ iObjectiveSee/src/Views/UILabel+iOS.m
@@ -12,18 +12,22 @@
 @implementation UILabel (iOS)
 
 - (void)sizeToFitSize:(CGSize)inSize {
-	CGSize tmpSize = [self.text sizeWithFont:self.font constrainedToSize:inSize lineBreakMode:self.lineBreakMode];
-	[self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, tmpSize.width, tmpSize.height)];
+    NSDictionary *attributDict = @{NSFontAttributeName: self.font};
+    CGRect tmpRect = [self.text boundingRectWithSize:inSize options:NSStringDrawingUsesLineFragmentOrigin attributes:attributDict context:nil];
+    
+	[self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, tmpRect.size.width, tmpRect.size.height)];
 }
 
 - (void)sizeToFitSizeMaintainWidth:(CGSize)inSize {
-	CGSize tmpSize = [self.text sizeWithFont:self.font constrainedToSize:inSize lineBreakMode:self.lineBreakMode];
-	[self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, tmpSize.height)];
+    NSDictionary *attributDict = @{NSFontAttributeName: self.font};
+	CGRect tmpRect = [self.text boundingRectWithSize:inSize options:NSStringDrawingUsesLineFragmentOrigin attributes:attributDict context:nil];
+	[self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, tmpRect.size.height)];
 }
 
 - (void)sizeToFitSizeMaintainHeight:(CGSize)inSize {
-	CGSize tmpSize = [self.text sizeWithFont:self.font constrainedToSize:inSize lineBreakMode:self.lineBreakMode];
-	[self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, tmpSize.width, self.frame.size.height)];
+    NSDictionary *attributDict = @{NSFontAttributeName: self.font};
+    CGRect tmpRect = [self.text boundingRectWithSize:inSize options:NSStringDrawingUsesLineFragmentOrigin attributes:attributDict context:nil];
+	[self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, tmpRect.size.width, self.frame.size.height)];
 }
 
 @end
